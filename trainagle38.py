22f7759c14e71d146c25902bdebf715907b7527e
siva50 <80094666+siva50@users.noreply.github.com>
2022-07-21 19:36:35 +0530

    Add files via upload

---
+++ trainagle38.py
@@ -0,0 +1,11 @@
+sides=int(input("enter any number"))
+if sides==3:
+    print("it is traingle")
+elif sides==4:
+    print("it is rectangle")
+elif sides==5:
+    print("it is pentangle")
+elif sides==6:
+    print("it is hexagonal")
+else:
+    print("it is out of range")
